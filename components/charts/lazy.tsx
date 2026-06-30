2af799d7dba7892ca9a077979c7378208d1fb338
Andres-GP <andresgarciapedreros@gmail.com>
2026-06-30 18:45:00 -0500

    implemented and updated linter

--- components/charts/lazy.tsx
+++ components/charts/lazy.tsx
@@ -1,33 +1,35 @@
-"use client"
+/* eslint-disable react/display-name */
 
-import dynamic from "next/dynamic"
-import { Skeleton } from "@/components/ui/skeleton"
+"use client";
+
+import dynamic from "next/dynamic";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const chartLoader = (height: number) => () => (
   <Skeleton className="w-full" style={{ height }} />
-)
+);
 
 export const EquityChart = dynamic(() => import("./equity-chart"), {
   ssr: false,
   loading: chartLoader(300),
-})
+});
 
 export const CapitalPie = dynamic(() => import("./capital-pie"), {
   ssr: false,
   loading: chartLoader(260),
-})
+});
 
 export const WeightsBar = dynamic(() => import("./weights-bar"), {
   ssr: false,
   loading: chartLoader(300),
-})
+});
 
 export const DrawdownChart = dynamic(() => import("./drawdown-chart"), {
   ssr: false,
   loading: chartLoader(260),
-})
+});
 
 export const MetricChart = dynamic(() => import("./metric-chart"), {
   ssr: false,
   loading: chartLoader(240),
-})
+});
